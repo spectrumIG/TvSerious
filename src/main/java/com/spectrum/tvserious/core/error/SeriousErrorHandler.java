a260f8df516e6db64f5f70234084efea1c89cfe4
Daniele Andreoli <cloudsky.it@gmail.com>
2013-02-04 20:06:23 +0100

    Implement first rudimental error handler ( 1 method)

---
+++ src/main/java/com/spectrum/tvserious/core/error/SeriousErrorHandler.java
@@ -0,0 +1,9 @@
+package com.spectrum.tvserious.core.error;
+
+public class SeriousErrorHandler {
+
+	public static String generateTvSeriousCliError(String cause){		
+		return "Error from TvRage! Try Again. Cause: " + cause;
+		
+	}
+}
