202c7ce305c0f4b2b39c81b1d09bca25e0faaec4
Daniele Andreoli <cloudsky.it@gmail.com>
2013-03-12 17:26:38 +0100

    Add value for SQL url and driver

--- src/main/java/com/spectrum/tvserious/core/Values.java
+++ src/main/java/com/spectrum/tvserious/core/Values.java
@@ -1,5 +1,8 @@
 package com.spectrum.tvserious.core;
 
-public class Values {
+public class Values {	
 	public static final String API_KEY = "iPLSpGH7bNC5tYGEbtxV";
+	
+	public static final String DB_DRIVER_SQLLITE = "org.sqlite.JDBC";
+	public static final String DB_URL_CONNECTION = "jdbc:sqlite:tvshow.db";
 }
